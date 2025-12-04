facdd5107f5866b944419ecc2a4f3ead334be071
Christina Kaden <christina.kaden@gmx.de>
2025-12-04 10:49:43 +0100

    fix for deployment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export const URL = window.location.href.includes("localhost")
+const URL = window.location.href.includes("localhost")
   ? "http://localhost:4040/api"
   : "https://wallpapers-by-alex.vercel.app";
+
+export default URL;
